5833c6e68715b9b8854581f12d190fedd00b05dc
CMeraz13 <meraz.cesar13@gmail.com>
2024-03-16 16:07:48 -0600

    slight change

--- Week 3/Prove/test_water_flow.py
+++ Week 3/Prove/test_water_flow.py
@@ -1,6 +1,7 @@
 from pytest import approx
 import pytest
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
+from water_flow import (water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, 
+                        pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction)
     
 
 
@@ -73,7 +74,7 @@ def test_pressure_loss_from_pipe_reduction():
     assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, abs=0.001)
     assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, abs=0.001)
 
-    
+
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
 pytest.main(["-v", "--tb=line", "-rN", __file__])
