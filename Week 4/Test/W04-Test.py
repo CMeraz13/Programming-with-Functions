a98ef8f5c164367d9bd7d54e5e4c806e212893b2
CMeraz13 <meraz.cesar13@gmail.com>
2024-03-19 15:40:05 -0600

    Took Test

---
+++ Week 4/Test/W04-Test.py
@@ -0,0 +1,25 @@
+metals={
+    "Ag" : "Silver",
+    "Al" : "Aluminum",
+    "Au" : "Gold",
+    "Cu" : "Copper",
+}
+
+if "Ag" in metals:
+    print("yes")
+else:
+    print("no")
+
+print(metals["Au"])
+
+"""
+Which statement can be used to see if the key Ag exists in the metals dictionary?
+
+A) metals has “Ag”
+
+B) metals contains “Ag”
+
+C) metals.exists(“Ag”)
+
+D) “Ag” in metals
+"""
