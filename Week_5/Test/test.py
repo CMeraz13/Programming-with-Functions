f4eaf42e3e42fde75a54c63320db7af6cb079b8c
CMeraz13 <98477297+CMeraz13@users.noreply.github.com>
2024-03-26 18:48:18 -0600

    Update test.py

--- Week_5/Test/test.py
+++ Week_5/Test/test.py
@@ -1,63 +1,63 @@
 """
 Q1) What two file types were discussed this week?
-  *  text  X
-  *  program O
+  *  text  O
+  *  program X
   *  data X
   *  binary O
 
 Q2) Which function is used to prepare a file for access in python.
-  * config O
-  * select O
-  * open X
-  * read O
+  * config X
+  * select X
+  * open O
+  * read X
 
 Q3) To read data from a comma separated value file into a 
     list you would need to import the ________ module .
-        * data O
-        * text O
-        * ascii O 
-        * csv X
+        * data X
+        * text X
+        * ascii X
+        * csv O
 
 Q4) To read the contents of a csv fine into a single variable you could use a ____________ 
     to store each row of the file (select all that apply).
-        * list X
-        * function O
-        * dictionary X
-        * variable O
+        * list O
+        * function X
+        * dictionary O
+        * variable X
 
 Q5) Which option would you use when preparing a file for use to be able to add 
     contents to the end of the file preserving the existing contents.
-        * wt O
-        * pt O
-        * rt O
-        * at X
+        * wt X
+        * pt X
+        * rt X
+        * at O
 
 6) A _____ occurs when an unanticipated condition occurs while the 
     program is running such as a file not found.
-        * halt O
-        * exception X
-        * logic error O
-        * syntax error O
+        * halt X
+        * exception O
+        * logic error X
+        * syntax error X
 
 Q7) The try statement is used to deal with errors. 
     Which code block of the try statement is executed regardless if an error occurs or not.
-        * exit O
-        * try O
-        * finally X
-        * else O
+        * exit X
+        * try X
+        * finally O
+        * else X
 
 Q8) Which of the following tasks would be most likely to 
     cause an error while the program is running.
-        * testing the value of a variable O
-        * storing a valie in a variable O
-        * reading from a file X
-        * printing the value of a variable O
+        * testing the value of a variable X
+        * storing a valie in a variable X
+        * reading from a file O
+        * printing the value of a variable X
 
 Q9) A try statement can have only 1 except block.
-    * True O
-    * False X
+    * True X
+    * False O
 
 Q10) Try statements should contain as much code as possible to account for anything that might go wrong.
-    * True O
-    * False X
+    * True X
+    * False O
 """
