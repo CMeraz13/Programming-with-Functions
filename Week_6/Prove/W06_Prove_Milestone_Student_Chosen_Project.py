7531d4b9840cfe5527e1e82eae786f1310780b29
CMeraz13 <meraz.cesar13@gmail.com>
2024-04-04 16:41:40 -0600

    Update W06_Prove_Milestone_Student_Chosen_Project.py

--- Week_6/Prove/W06_Prove_Milestone_Student_Chosen_Project.py
+++ Week_6/Prove/W06_Prove_Milestone_Student_Chosen_Project.py
@@ -27,35 +27,98 @@ W06 Prove Milestone: Student Chosen Program:
 
 """
 
-import math
-import random
+import math, random, time, sys, os
 from datetime import datetime
-import time
+
+def typingPrint(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.05)
 
 def main():
     try:
         print()
         print()
 
-        print("Welcome to Fantasy Adventure: Demo!")
+        typingPrint("Welcome to Fantasy Adventure: Demo!")
         time.sleep(2)
-        print("Before we begin, you will need a character!")
-        time.sleep(3)
-        print("Lets take a look at the Level 1: Character Stats!")
+        typingPrint("\nBefore we begin, you will need a character!")
+        typingPrint("\nLets take a look at the Stats!")
         time.sleep(2)
+        print()
 
         display_standard_stats = Character_Stat()
         print(display_standard_stats)
 
-        print("As you can see, each character has 6 individual stats")
+        typingPrint("\nEach character has 5 individual stats.")
         time.sleep(2)
-        print("Each individual stat will affect what your character can and cant do")
-        time.sleep()
-        print("Health. Health indicates how many hit point your character has before you get a game over.")
-        time.sleep()
-        print("If your health points reach 0, its Game Over.")
-        time.sleep()
+        typingPrint("\nEach individual stat will affect what your character can and cant do.")
+        time.sleep(2)
+        print()
+        typingPrint("\nHealth:")
+        time.sleep(2)
+        typingPrint("\nHealth indicates how many 'Hit Points' or 'HP' your character has before you get a game over.")
+        time.sleep(2)
+        typingPrint("\nIf your 'HP' reach 0, its 'Game Over'.")
+        time.sleep(2)
+        typingPrint("\nDuring the game you will encounter Enemies who will cause you to take damange or get hit")
+        time.sleep(2)
+        typingPrint("\nSo be sure to always be careful or you will reach 'Game Over'!")
+        time.sleep(2)
+        print()
+        typingPrint("\nStrength:") 
+        time.sleep(2)
+        typingPrint("\nStrength is one of the stats that will determine how much 'Physical Damage'")
+        time.sleep(2)
+        typingPrint("\nyour character will do and how much 'Physical Resistances' protect you from 'Physical Damage'.")
+        time.sleep(2)
+        typingPrint("\nStrength:")
+        time.sleep(2)
+        typingPrint("\nDexterity:")
+        time.sleep(2)
+        typingPrint("\nDexterity is the second of the stats that will determine how much 'Physical Damage'")
+        time.sleep(2)
+        typingPrint("\nyour character will do and how much 'Physical Resistances' protect you from 'Physical Damage'.")
+        time.sleep(2)
+        typingPrint("\nIntelligence:")
+        time.sleep(2)
+        typingPrint("\nIntelligences is one of the stats that will determine how much 'Magic Damage'")
+        time.sleep(2)
+        typingPrint("\nyour character will do and how much 'Magic Resistance' will protect you from 'Magic Damage'.")
+        time.sleep(2)
+        typingPrint("\nFaith:")
+        time.sleep(2)
+        typingPrint("\nFaith is the stat that will determine how much 'Holy Damage'")
+        time.sleep(2)
+        typingPrint("\nyour character will do and how much 'Holy Resistance' will protect you from 'Holy Damage'.")
+        time.sleep(2)
+        typingPrint("\nLets take a look at the Level 1: Character Stats!")
+        time.sleep(2)
+        health = 100
+        # player_health = health * (allocated_health_points * 10)
+
+        #mana = 50
+        #player_mana = mana * (allocated_mana_points * 3.2)
+
+        strength = 1
+        #player_strength = strength + allocated_strength_points
+
+        dexterity = 1
+        #player_dexterity = dexterity + allocated_dexterity_points
+
+        intelligence = 1
+        #player_intelligence = intelligence + allocated_intelligence_points
+
+        faith = 1
+        #player_faith = faith + allocated_faith_points
         
+        print()
+        print(f"Character stats: \nHealth: {health}\nStrength: {strength}\nDexterity: {dexterity}\nIntelligence: {intelligence}\nFaith: {faith}")
+
+        print()
+        print()
+
 
 
 
@@ -68,30 +131,14 @@ def main():
 
 def Character_Stat():
 
-    # The purpose of this function is to determine what stats the players character will have
+    allocate_points = 37
 
-    health = 100
-    # player_health = health * (allocated_health_points * 10)
+    while allocate_points != 0 or allocate_points = 37:
+        
 
-    mana = 50
-    #player_mana = mana * (allocated_mana_points * 3.2)
-
-    strength = 1
-    #player_strength = strength + allocated_strength_points
-
-    dexterity = 1
-    #player_dexterity = dexterity + allocated_dexterity_points
-
-    intelligence = 1
-    #player_intelligence = intelligence + allocated_intelligence_points
-
-    faith = 1
-    #player_faith = faith + allocated_faith_points
+def clearScreen():
+  os.system("clear")
     
-
-    print(f"Character stats: \nHealth: {health}\nMana: {mana}\nStrength: {strength}\nDexterity: {dexterity}\nIntelligence: {intelligence}\nFaith: {faith}")
-
-
 
 
 
