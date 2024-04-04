b7ad1e6dd0ff6acb369c0760c088fcb2902530a5
CMeraz13 <meraz.cesar13@gmail.com>
2024-04-03 19:36:39 -0600

    Update W06_Prove_Milestone_Student_Chosen_Project.py

--- Week_6/Prove/W06_Prove_Milestone_Student_Chosen_Project.py
+++ Week_6/Prove/W06_Prove_Milestone_Student_Chosen_Project.py
@@ -30,11 +30,39 @@ W06 Prove Milestone: Student Chosen Program:
 import math
 import random
 from datetime import datetime
+import time
 
 def main():
-    
-    print("Welcome to Fantasy Adventure: Demo!")
+    try:
+        print()
+        print()
 
+        print("Welcome to Fantasy Adventure: Demo!")
+        time.sleep(2)
+        print("Before we begin, you will need a character!")
+        time.sleep(3)
+        print("Lets take a look at the Level 1: Character Stats!")
+        time.sleep(2)
+
+        display_standard_stats = Character_Stat()
+        print(display_standard_stats)
+
+        print("As you can see, each character has 6 individual stats")
+        time.sleep(2)
+        print("Each individual stat will affect what your character can and cant do")
+        time.sleep()
+        print("Health. Health indicates how many hit point your character has before you get a game over.")
+        time.sleep()
+        print("If your health points reach 0, its Game Over.")
+        time.sleep()
+        
+
+
+
+
+
+    except:
+        print()
 
     print()
 
@@ -43,18 +71,28 @@ def Character_Stat():
     # The purpose of this function is to determine what stats the players character will have
 
     health = 100
-    endurance = 10
-    mana = 50
-    strength = 1
-    dexterity = 1
-    intelligence = 1
-    faith = 1
+    # player_health = health * (allocated_health_points * 10)
 
+    mana = 50
+    #player_mana = mana * (allocated_mana_points * 3.2)
+
+    strength = 1
+    #player_strength = strength + allocated_strength_points
+
+    dexterity = 1
+    #player_dexterity = dexterity + allocated_dexterity_points
+
+    intelligence = 1
+    #player_intelligence = intelligence + allocated_intelligence_points
+
+    faith = 1
+    #player_faith = faith + allocated_faith_points
     
 
-    print(f"Character stats: \nHealth: {health}\nEndurance: {endurance}\nMana: {mana}\nStrength: {strength}\nDexterity: {dexterity}\nIntelligence: {intelligence}\nFaith: {faith}")
+    print(f"Character stats: \nHealth: {health}\nMana: {mana}\nStrength: {strength}\nDexterity: {dexterity}\nIntelligence: {intelligence}\nFaith: {faith}")
 
 
 
 
-Character_Stat()
+
+main()
