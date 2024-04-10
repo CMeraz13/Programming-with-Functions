b1c4b10076c6d56c952fd6bc21f1d6f5c0b74bfc
CMeraz13 <meraz.cesar13@gmail.com>
2024-04-09 23:48:33 -0600

    added a new file

---
+++ Week_7/Reflections/W07_Reflection.py
@@ -0,0 +1,24 @@
+"""
+W07: Reflection
+    Congratulations! You successfully made it to the final lesson of CSE 111.
+
+    During this course you learned to write programs organized with functions to solve significant problems in a variety of domains, which was the major outcome of this course. In addition, you learned to do the following:
+
+        Research modules, objects, and functions written by others and use them in your programs.
+        Write programs that can detect and recover from invalid conditions (exceptions).
+        Follow good practices in designing, writing, and debugging functions, including writing each function so 
+        that it performs one task only and writing test functions to verify that program functions are correct.
+
+    Each of these skills will transfer to other programming languages that you learn in your future. 
+    Also, researching modules, objects, and functions written by others gave you important practice in lifelong learning. 
+    Even though the work of this course may have been challenging at times, you showed skill in thinking analytically, 
+    breaking down problems into manageable pieces, working with others, and debugging abstract problems. 
+    These are skills that will transfer to any field of study or career path that you choose.
+
+    Instructions:
+        1) Download the Reflection Document Template.
+        2) Answer each question thoroughly (at least 3 sentences each).
+        3) Return to Canvas to:
+            A) Submit your reflection document.
+            B) Take the outcome self-assessment quiz.
+"""
