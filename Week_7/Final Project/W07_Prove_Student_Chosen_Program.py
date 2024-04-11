ad0b68443c9b745aa2e28d5eb64ff900766b3e33
CMeraz13 <meraz.cesar13@gmail.com>
2024-04-10 23:18:36 -0600

    added final file

---
+++ Week_7/Final Project/W07_Prove_Student_Chosen_Program.py
@@ -0,0 +1,28 @@
+"""
+W07 Prove: Student Chosen Program
+    Purpose:
+        Prove that you can write a significant Python project that solves a real-world 
+        problem and is well organized with functions.
+
+    Assignment:
+        Finish developing the Student Chosen Program program that you started in week 06. 
+        Your program must include multiple functions that you verify are correct with test functions and pytest.
+
+    Submission:
+        In order to receive credit for this prove assignment, you must submit your program and a 
+        report that describes your work on your program. Do the following:
+
+        1) At the beginning of this week, download and save this project-report.txt file.
+        2) Open the downloaded project-report.txt file in VS Code. 
+            Notice that question #1 in the file requires you to record in a table the time that you work on your program. 
+            In order to receive full points for the project, you must work on your program for at least a 
+            total of 12 hours (over the whole project). 
+            As you work on your program during this week, be certain to record the dates and 
+            times and a description of your work.
+        3) Before you complete your program review the assignment rubric in 
+            Canvas the grader will use to score your project-report.txt file and your program.
+        4) Before the due date submit your completed project-report.txt file, your Python program, your Python test file, 
+            and any data files such as .txt and .csv files to the W07 Prove Assignment in Canvas
+
+    If your project requires non standard libraries or complex setups, such as a database or external API. Please create a short video that demonstrates your program running. Explain your usage of the external components and how they are used in your program.
+"""
